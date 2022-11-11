95f221ef397bd1c0e2877c6381d309710989b416
Selim Eraslan <116980612+wanheda06@users.noreply.github.com>
2022-11-11 14:30:14 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int liczby1 = 20;
+        int liczby2 = 18;
+        int liczby3 = 22;
+        int wieksza = liczby1;
+
+
+        if (wieksza < liczby2) {
+            wieksza = liczby2;
+        }
+
+        if (wieksza < liczby3) {
+            wieksza = liczby3;
+        }
+
+        System.out.println("większa liczba jest: " + wieksza);
+    }
+}
